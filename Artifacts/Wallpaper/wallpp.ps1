ca73be34146362486c074613a49767dfd491b806
harshi2 <harshi.raj26@gmail.com>
2017-06-04 22:49:52 +0530

    changes

--- Artifacts/Wallpaper/wallpp.ps1
+++ Artifacts/Wallpaper/wallpp.ps1
@@ -1,3 +1,3 @@
-﻿reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Windows\Web\Wallpaper\Windows\img2.jpg /f 
+﻿reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Windows\Web\Wallpaper\Windows\img1.jpg /f 
 
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
